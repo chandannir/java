829c1ace4d4f05888328ae444b54f910a2513a3e
Chandan Nir <chandannir82@gmail.com>
2024-08-10 17:53:49 -0400

    first object

--- java-core/fist-file.java
+++ java-core/fist-file.java
@@ -1,12 +1,14 @@
 class first{
 
- static int a = 3;
- static int b = 2;
- static int c = b + a;
+  static int a = 3;
+  static int b = 2;
+  static int c = b + a;
 
     public static void main(String[] args) {
         
         System.out.println(c);
+        first o1 = new first();
+        System.out.println(o1);
 
     }
 }

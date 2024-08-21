f114ad692f641a3edcbb511215ff1b9afaa15c1f
Chandan Nir <chandannir82@gmail.com>
2024-08-21 15:15:33 -0400

    constructors

--- java-core/constructors/constructors.java
+++ java-core/constructors/constructors.java
@@ -4,7 +4,21 @@ package constructors;
 
 public class constructors {
 
-    constructors(){} //constructor
+    int a;
+    constructors(){
 
+        int a = 3;
+    } //constructor
+
+    public static void main(String[] args) {
+        constructors myObj = new constructors();
+        System.out.println(myObj.a);
+
+        /*
+        If we didn't include the constructor to intialize the value
+        variable a the SOP statement would not work.
+         */
+
+    }
 
 }

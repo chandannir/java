ee0acc3d01b5230858dc0897b0aafaff34595100
Chandan Nir <chandannir82@gmail.com>
2024-08-22 21:28:10 -0400

    java course from udemy

---
+++ java-course/src/IfThenStament.java
@@ -0,0 +1,14 @@
+public class IfThenStament {
+
+
+    public static void main(String[] args) {
+
+
+
+        boolean isAlien = false;
+        if(isAlien == false){
+            System.out.println("It is not an alien");
+
+        }
+    }
+}
